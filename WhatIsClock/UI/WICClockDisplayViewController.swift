942025e0b54c75d1ac981d142ed2f335c9a8a1c8
liyunkai <jfunsir@gmail.com>
2015-11-23 21:11:52 +0800

    viewcontroller不会初始化，日狗

--- WhatIsClock/UI/WICClockDisplayViewController.swift
+++ WhatIsClock/UI/WICClockDisplayViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class WICClockDisplayViewController: UIViewController {
+    
+    var scrollView:UIScrollView
+    var viewL, viewM, viewR : UIView
+    
+    let SCREEN_WIDTH = UIScreen.mainScreen().bounds.width
+    let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.height
+    let CLOCK_TAB_NUM = 3
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        self.scrollView = UIScrollView(frame: self.view.frame)
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+    }
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    convenience override init(){
+        self.init(nibName: nil, bundle:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.contentSize = CGSize(width: SCREEN_WIDTH * CGFloat(CLOCK_TAB_NUM), height: SCREEN_HEIGHT)
+        
     }
 
     override func didReceiveMemoryWarning() {
