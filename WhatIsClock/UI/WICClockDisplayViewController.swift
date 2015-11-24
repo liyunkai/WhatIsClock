d5f7c2dbe6f5fb4edd6897331a0598fb8e423080
李允恺(Yunkai Li) <jfunsir@gmail.com>
2015-11-24 23:03:31 +0800

    初始化不报错了，参考初始化3原则

--- WhatIsClock/UI/WICClockDisplayViewController.swift
+++ WhatIsClock/UI/WICClockDisplayViewController.swift
@@ -11,25 +11,25 @@ import UIKit
 class WICClockDisplayViewController: UIViewController {
     
     var scrollView:UIScrollView
-    var viewL, viewM, viewR : UIView
+//    var viewL, viewM, viewR : UIView
     
     let SCREEN_WIDTH = UIScreen.mainScreen().bounds.width
     let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.height
     let CLOCK_TAB_NUM = 3
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        self.scrollView = UIScrollView(frame: self.view.frame)
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        
-    }
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+//        self.scrollView = UIScrollView(frame: UIScreen.mainScreen().bounds)
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//    }
 
     required init?(coder aDecoder: NSCoder) {
+        self.scrollView = UIScrollView(frame: UIScreen.mainScreen().bounds)
         super.init(coder: aDecoder)
     }
     
-    convenience override init(){
-        self.init(nibName: nil, bundle:nil)
-    }
+//    convenience init(){
+//        self.init(nibName: nil, bundle:nil)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
