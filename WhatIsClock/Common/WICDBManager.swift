f63aaa122b7b4cf5e840ac017f813583340a44fe
李允恺(Yunkai Li) <jfunsir@gmail.com>
2015-12-20 11:50:49 +0800

    db的操作，insert是语法写错了。而出现奇怪错误是代码改变想改变数据库结构，如增加列等，但数据库不能这样操作，要用数据库语句，因此造成一些奇怪错误。删掉数据库重新Run即可

--- WhatIsClock/Common/WICDBManager.swift
+++ WhatIsClock/Common/WICDBManager.swift
@@ -18,18 +18,19 @@ class WICDBManager {
     static let TNAME_SCHEDUAL_CLOCKS = "test_sche"//"TNAME_SCHEDUAL_CLOCKS"
     
     static let notifID = Expression<Int64>("notifID")
-    static let isTaskOn = Expression<Bool>("isTaskOn")
-    static let isVerbose = Expression<Bool>("isVerbose")
+    static let isTaskOn = Expression<Bool?>("isTaskOn")
+    static let isVerbose = Expression<Bool?>("isVerbose")
     static let fireDate = Expression<Int64>("fireDate")
-    static let soundName = Expression<String>("soundName")
-    static let message = Expression<String>("message")
-    static let loop = Expression<String>("loop")
+    static let soundName = Expression<String?>("soundName")
+    static let message = Expression<String?>("message")
+    static let loop = Expression<String?>("loop")
     
     static let g_db:Connection? = {
         let path = NSSearchPathForDirectoriesInDomains(
                     .DocumentDirectory, .UserDomainMask, true
                     ).first!
         let db =  try! Connection("\(path)/db.sqlite3")//这里crash了怎么办内
+        print(path)
         return db
     }()
     
@@ -70,10 +71,42 @@ class WICDBManager {
             return
         }
         let table = Table(tableName)
-        let clockInsert = Insert(soundName <- "aa")
-        let rowId = try! db.run(clockInsert)
+        let clockInsert = table.insert(notifID <- 1, fireDate <- 128246, soundName <- "aa", message <- "gagaga")
+        do{
+            let rowId = try db.run(clockInsert)
+            print(rowId)
+        }catch{
+            print(error)
+        }
+        for clock in db.prepare(table){
+            print("sound \(clock[soundName]) isverbose  firedate \(clock[fireDate]) notifID \(clock[notifID])")
+        }
     }
 
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WhatIsClock/UI/WICClockSettingViewController.swift
+++ WhatIsClock/UI/WICClockSettingViewController.swift
@@ -19,7 +19,7 @@ class WICClockSettingViewController: UITableViewController, MPMediaPickerControl
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        selectRing()
+//        selectRing()
         
     }
     
